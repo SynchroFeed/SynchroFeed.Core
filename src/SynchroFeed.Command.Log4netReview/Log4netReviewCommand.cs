629687018154eab24e9d268d5c57beba60832353
Andrew Rissing <arissing@alkamitech.com>
2019-03-30 21:25:59 -0500

    Added Log4netReview Command.

---
+++ src/SynchroFeed.Command.Log4netReview/Log4netReviewCommand.cs
@@ -0,0 +1,234 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using Microsoft.Extensions.Logging;
+using SynchroFeed.Library.Action;
+using SynchroFeed.Library.Command;
+using SynchroFeed.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Settings = SynchroFeed.Library.Settings;
+
+namespace SynchroFeed.Command.Log4netReview
+{
+    /// <summary>
+    /// The Log4netReviewCommand class is a <see cref="Command"/> that validates
+    /// that the Package has valid log4net configuration.
+    /// </summary>
+    public class Log4netReviewCommand : BaseCommand
+    {
+        private const string Setting_PackageIdRegex = "PackageIdRegex";
+        private const string Setting_ConversionPattern = "ConversionPattern";
+
+        private enum LogLevel
+        {
+            Off = 0,
+            Fatal,
+            Error,
+            Warn,
+            Info,
+            Debug,
+            Trace,
+            All,
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Log4netReviewCommand" /> class.
+        /// </summary>
+        /// <param name="action">The action running with this command.</param>
+        /// <param name="commandSettings">The command settings associated with this command.</param>
+        /// <param name="loggerFactory">The logger factory.</param>
+        /// <exception cref="ArgumentNullException">loggerFactory</exception>
+        public Log4netReviewCommand(
+            IAction action,
+            Settings.Command commandSettings,
+            ILoggerFactory loggerFactory)
+            : base(action, commandSettings, loggerFactory)
+        {
+            if (loggerFactory == null) throw new ArgumentNullException(nameof(loggerFactory));
+            Logger = loggerFactory.CreateLogger<Log4netReviewCommand>();
+        }
+
+        public override string Type => "Log4netReview";
+
+        /// <summary>
+        /// Gets the logger.
+        /// </summary>
+        /// <value>The logger.</value>
+        private ILogger Logger { get; }
+
+        /// <summary>
+        /// Validates that the specified package has additional support files.
+        /// </summary>
+        /// <param name="package">The package to examine.</param>
+        /// <param name="packageEvent">The event associated with the package.</param>
+        /// <returns>Returns <c>true</c> if the packages has valid log4net configuration, otherwise <c>false</c>.</returns>
+        public override CommandResult Execute(Package package, PackageEvent packageEvent)
+        {
+            if (package == null)
+                throw new ArgumentNullException(nameof(package));
+
+            if (packageEvent != PackageEvent.Added)
+            {
+                Logger.LogTrace($"Ignoring {package.Id} due to the event being a delete");
+                return new CommandResult(this);
+            }
+
+            if (package.Content == null)
+            {
+                Logger.LogWarning($"Contents are empty for package {package}.");
+                return new CommandResult(this);
+            }
+
+            if (this.Settings.Settings.TryGetValue(Setting_PackageIdRegex, out var packageIdRegex) && !string.IsNullOrWhiteSpace(packageIdRegex))
+            {
+                if (!Regex.IsMatch(package.Id, packageIdRegex, RegexOptions.IgnoreCase))
+                {
+                    Logger.LogTrace($"{package.Id} (v{package.Version}) skipped due to package filtering.");
+                    return new CommandResult(this, true, $"{package} package check skipped.");
+                }
+            }
+            else
+            {
+                Logger.LogTrace($"Command does not have a '{Setting_PackageIdRegex}' setting, defaulting to all packages.");
+            }
+
+            var issues = new List<string>();
+
+            using (var byteStream = new MemoryStream(package.Content))
+            using (var zipFile = new ZipFile(byteStream))
+            {
+                foreach (ZipEntry zipEntry in zipFile)
+                {
+                    if (!zipEntry.IsFile || !zipEntry.Name.EndsWith(".config"))
+                        continue;
+
+                    try
+                    {
+                        using (Stream zipStream = zipFile.GetInputStream(zipEntry))
+                        {
+                            using (var memoryStream = new MemoryStream((int)zipEntry.Size))
+                            {
+                                zipStream.CopyTo(memoryStream);
+                                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                                var doc = XDocument.Load(memoryStream);
+
+                                ParseConfig(zipEntry.Name, doc, issues);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.LogInformation(e, "Unable to parse: {0}", zipEntry.Name);
+                    }
+                }
+            }
+
+            if (issues.Count < 1)
+            {
+                var message = $"{package.Id} (v{package.Version}) has valid log4net configuration.";
+
+                Logger.LogTrace(message);
+
+                return new CommandResult(this, true, message);
+            }
+            else
+            {
+                var message = $"{package.Id} (v{package.Version}) has invalid log4net configuration: {string.Join(", ", issues)}";
+
+                Logger.LogWarning(message);
+
+                return new CommandResult(this, false, message);
+            }
+        }
+
+        private void ParseConfig(string fileName, XDocument doc, List<string> issues)
+        {
+            // Verify the file is a log4net config file.
+            if (doc.XPathSelectElement(@"/configuration/log4net") == null)
+                return;
+
+            foreach (var xpath in GetXPathSettings())
+            {
+                if (this.Settings.Settings.TryGetValue(xpath, out var logLevelValue) && !string.IsNullOrWhiteSpace(logLevelValue))
+                {
+                    var elements = FindElementsFromXpath(doc, xpath);
+
+                    if (elements.Count > 0)
+                    {
+                        var availableLogLevels = GetAvailableLogLevels(logLevelValue);
+
+                        foreach (var element in elements)
+                        {
+                            var currentLogLevel = ParseLogLevel(element?.Attribute("value")?.Value);
+
+                            if ((currentLogLevel != null) && !availableLogLevels.Contains(currentLogLevel.Value))
+                            {
+                                issues.Add($"{fileName}: '{currentLogLevel}' is not a valid log level for {xpath}");
+                            }
+                        }
+                    }
+                }
+            }
+
+            /**************************************/
+
+            if (this.Settings.Settings.TryGetValue(Setting_ConversionPattern, out var expectedConversionPattern) && !string.IsNullOrWhiteSpace(expectedConversionPattern))
+            {
+                var conversionPatterns = doc.XPathSelectElements(@"/configuration/log4net/appender/layout/conversionPattern[@value]");
+
+                foreach (var conversionPattern in conversionPatterns)
+                {
+                    var pattern = conversionPattern.Attribute("value")?.Value.Trim();
+
+                    if (!string.Equals(pattern, expectedConversionPattern, StringComparison.InvariantCultureIgnoreCase))
+                        issues.Add($"{fileName}: 'conversionPattern' does not match expected pattern");
+                }
+            }
+            else
+            {
+                Logger.LogDebug("{0} test skipped, due to configuration not being set.", Setting_ConversionPattern);
+            }
+        }
+
+        private List<string> GetXPathSettings()
+        {
+            return this.Settings.Settings.Keys
+                .Where(key => !string.IsNullOrWhiteSpace(key)
+                    && !string.Equals(key, Setting_ConversionPattern, StringComparison.InvariantCultureIgnoreCase)
+                    && !string.Equals(key, Setting_PackageIdRegex, StringComparison.InvariantCultureIgnoreCase))
+                .ToList();
+        }
+
+        private List<XElement> FindElementsFromXpath(XDocument doc, string xpath)
+        {
+            var finalXPath = $"{xpath.TrimEnd('/')}/level[@value]";
+
+            return doc
+                .XPathSelectElements(finalXPath)
+                .ToList();
+        }
+
+        private static List<LogLevel> GetAvailableLogLevels(string logLevelValue)
+        {
+            return logLevelValue
+                .Split('|')
+                .Select(ParseLogLevel)
+                .Where(level => level != null)
+                .Select(level => level.Value)
+                .ToList();
+        }
+
+        private static LogLevel? ParseLogLevel(string value)
+        {
+            if ((value == null) || !Enum.TryParse<LogLevel>(value, true, out var level))
+                return null;
+
+            return level;
+        }
+    }
+}
---
+++ test/SynchroFeed.Command.Log4netReview.Test/Log4netReviewTest.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SynchroFeed.Library.Factory;
+using SynchroFeed.Library.Model;
+using SynchroFeed.Library.Settings;
+using SynchroFeed.Library.TestFramework;
+using System;
+using System.IO;
+using Xunit;
+
+namespace SynchroFeed.Command.Log4netReview.Test
+{
+    public class Log4netReviewTest
+    {
+        private ILoggerFactory LoggerFactory { get; }
+        private TestLogger Logger { get; } = new TestLogger();
+        private string RootFolder { get; }
+
+        public Log4netReviewTest()
+        {
+            RootFolder = Environment.CurrentDirectory;
+
+            var mockLoggerFactory = new Mock<ILoggerFactory>();
+            mockLoggerFactory
+                .Setup(factory => factory.CreateLogger(It.IsAny<string>()))
+                .Returns(Logger);
+
+            LoggerFactory = mockLoggerFactory.Object;
+        }
+
+        [Fact]
+        public void Factory_Test()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton(LoggerFactory);
+            serviceCollection.AddSingleton(new ApplicationSettings());
+            serviceCollection.AddTransient(typeof(ICommandFactory), typeof(Log4netReviewCommandFactory));
+
+            var serviceProvider = serviceCollection
+                .BuildServiceProvider();
+
+            var sut = serviceProvider.GetRequiredService<ICommandFactory>();
+
+            Assert.NotNull(sut);
+            Assert.IsType<Log4netReviewCommandFactory>(sut);
+            Assert.IsAssignableFrom<ICommandFactory>(sut);
+        }
+
+        [Theory]
+        [InlineData("^Test.*", false)]
+        [InlineData("^Package.*", true)]
+        public void PackageId_Regex_Test(string packageIdRegex, bool expectedResult)
+        {
+            var commandResult = CommandTestWrapper.Execute
+            (
+                (action, command, loggerFactory) => new Log4netReviewCommand(action, command, loggerFactory),
+                LoggerFactory,
+                Path.Combine(RootFolder, "local.choco", "Test.Package.1.0.0.nupkg"),
+                new Library.Settings.Command()
+                {
+                    Settings =
+                    {
+                        { "PackageIdRegex", packageIdRegex },
+                        { "/configuration/log4net/root", "OFF" },
+                    }
+                },
+                PackageEvent.Added
+            );
+
+            Assert.Equal(expectedResult, commandResult.ResultValid);
+        }
+
+        [Theory]
+        [InlineData("/configuration/log4net/root", "trace|warn|off", PackageEvent.Added, false)]
+        [InlineData("/configuration/log4net/root", "TRACE|WARN|OFF", PackageEvent.Added, false)]
+        [InlineData("/configuration/log4net/root", "trace|warn|off|info", PackageEvent.Added, true)]
+        [InlineData("/configuration/log4net/root", "TRACE|WARN|OFF|INFO", PackageEvent.Added, true)]
+        [InlineData("/configuration/log4net/logger[@name='LogInfo']", "TRACE|WARN|OFF|INFO", PackageEvent.Added, true)]
+        [InlineData("/configuration/log4net/logger[@name='LogInfo']", "TRACE|WARN|OFF", PackageEvent.Added, false)]
+        [InlineData("/configuration/log4net/logger[starts-with(@name, 'Log')]", "INFO|WARN|ERROR", PackageEvent.Added, true)]
+        [InlineData("/configuration/log4net/logger[starts-with(@name, 'Log')]", "INFO|WARN", PackageEvent.Added, false)]
+        [InlineData("/configuration/log4net/root", "TRACE|WARN|OFF", PackageEvent.Deleted, true)]
+        public void General_Test(string key, string value, PackageEvent packageEvent, bool expectedResult)
+        {
+            var commandResult = CommandTestWrapper.Execute
+            (
+                (action, command, loggerFactory) => new Log4netReviewCommand(action, command, loggerFactory),
+                LoggerFactory,
+                Path.Combine(RootFolder, "local.choco", "Test.Package.1.0.0.nupkg"),
+                new Library.Settings.Command()
+                {
+                    Settings =
+                    {
+                        { key, value },
+                    }
+                },
+                packageEvent
+            );
+
+            Assert.Equal(expectedResult, commandResult.ResultValid);
+        }
+
+        [Theory]
+        [InlineData("%date |*| %thread |*| %-5level |*| %logger |*| %message%newline", true)]
+        [InlineData("%date |*| %thread |*| %-5level |*| %logger |*| %message%newline%newline", false)]
+        public void ConversionPattern_Test(string conversionPattern, bool expectedResult)
+        {
+            var commandResult = CommandTestWrapper.Execute
+            (
+                 (action, command, loggerFactory) => new Log4netReviewCommand(action, command, loggerFactory),
+                 LoggerFactory,
+                 Path.Combine(RootFolder, "local.choco", "Test.Package.1.0.0.nupkg"),
+                 new Library.Settings.Command()
+                 {
+                     Settings =
+                     {
+                         { "ConversionPattern", conversionPattern },
+                     }
+                 },
+                 PackageEvent.Added
+            );
+
+            Assert.Equal(expectedResult, commandResult.ResultValid);
+        }
+    }
+}
